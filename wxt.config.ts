da36866ba00d90f63fc92cc9cfc7e7460fba144d
TomariRyusei <t.ryu6635@gmail.com>
2025-08-26 21:57:09 +0900

    アイコンを追加

--- wxt.config.ts
+++ wxt.config.ts
@@ -14,6 +14,22 @@ export default defineConfig({
   },
 
   manifest: {
+    icons: {
+      "16": "icon-16.png",
+      "32": "icon-32.png",
+      "48": "icon-48.png",
+      "96": "icon-96.png",
+      "128": "icon-128.png",
+    },
+    action: {
+      default_icon: {
+        "16": "icon-16.png",
+        "32": "icon-32.png",
+        "48": "icon-48.png",
+        "96": "icon-96.png",
+        "128": "icon-128.png",
+      },
+    },
     content_scripts: [
       {
         matches: ["<all_urls>"],
